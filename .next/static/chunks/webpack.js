4db6933bb7810299ac5319ddf620770374a98582
wostrau <a.ostrovtsov@gmail.com>
2023-06-28 23:59:08 +0200

    #383 - DONE

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6366c960dec9d982a11c";
+/******/ 	var hotCurrentHash = "297d86a1c6ef381caf72";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
